86db1fe45452436f2f09df49a81c86403ebcdd8e
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-02-04 10:55:30 -0500

    Adding begin/rescue to player initialize method so it now works and does not break due to additional attributes in hash which aren't attr_accessor's

--- FantasyNBA/lib/player.rb
+++ FantasyNBA/lib/player.rb
@@ -4,7 +4,9 @@ class FantasyNBA::Player
 
   def initialize(player_hash)
     player_hash.each do |key, value|
+      begin
         self.send("#{key}=", value)
+      rescue 
       end 
     end
   end
